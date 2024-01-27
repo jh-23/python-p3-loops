2d4848b343f56568ec3525f5a88e1b3bc1a4d644
John Heger <heger7@gmail.com>
2024-01-27 15:11:37 -0600

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,30 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    # for i in range(10, 0, -1):
+    #     print(i)
+    #     print("Happy New Year!")
+    
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_integers = [num**2 for num in int_list]
+    return squared_integers
 
 def fizzbuzz():
     # code goes here!
-    pass
+    i = 0
+    while i < 100:
+        i += 1
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz") 
+        elif i % 3 == 0:
+           print("Fizz")
+        elif i % 5 == 0: 
+            print("Buzz")
+        else:
+            print(i)
